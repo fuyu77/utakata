1be83f4fc1825335068a12f9b78648ab8cd01cae
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-02 22:22:17 +0900

    Modify production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.read_encrypted_secrets = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: Rails.application.secrets.gmail_user_name,
+    password: Rails.application.secrets.gmail_password,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
