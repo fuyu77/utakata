c4b39b1f0038c07de736c18464a4a623ef7b10d5
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-03 10:33:49 +0900

    Modify mailer config for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,14 +91,14 @@ Rails.application.configure do
 
   config.read_encrypted_secrets = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'utakatanka.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['GMAIL_USER_NAME'],
     password: ENV['GMAIL_PASSWORD'],
-    authentication: :plain,
+    authentication: 'login',
     enable_starttls_auto: true
   }
 end
