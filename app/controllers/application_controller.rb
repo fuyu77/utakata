1d2e00fb1573c17d0a67c434ce52f2f5323f18cb
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-08 21:49:59 +0900

    Set redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :ensure_domain
+  
+  FQDN = 'utakatanka.js'
  
+  def ensure_domain
+   return unless /\.herokuapp.com/ =~ request.host
+   port = ":#{request.port}" unless [80, 443].include?(request.port)
+   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+
   protected
  
   def configure_permitted_parameters
