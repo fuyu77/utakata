22889d2d2a5b93bccd54609e3f9f0ea10f2aed5b
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-08 21:52:14 +0900

    Tweak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :ensure_domain
   
-  FQDN = 'utakatanka.js'
+  FQDN = 'utakatanka.jp'
  
   def ensure_domain
    return unless /\.herokuapp.com/ =~ request.host
