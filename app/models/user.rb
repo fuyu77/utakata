549571713ef7b7ddd7936c08538dc0016725450c
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-13 17:41:58 +0900

    Modify layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
   if Rails.env == 'production'
-    has_attached_file :avatar, styles: { original: "80x80>", medium: "35x35>", small: "20x20>" }, default_url: "http://utakata.s3.amazonaws.com/:style/utakata.png"
+    has_attached_file :avatar, styles: { original: "75x75>", medium: "35x35>", small: "20x20>" }, default_url: "http://utakata.s3.amazonaws.com/:style/utakata.png"
   else
-    has_attached_file :avatar, styles: { original: "80x80>", medium: "35x35>", small: "20x20>" }, default_url: "/:style/utakata.png"
+    has_attached_file :avatar, styles: { original: "75x75>", medium: "35x35>", small: "20x20>" }, default_url: "/:style/utakata.png"
   end
   
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
