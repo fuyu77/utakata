ce3722d09ebd5160e1e80fa2681c3a10c4e30068
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-18 23:38:45 +0900

    設定編集後にホーム画面に移動しないように変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -65,4 +65,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path
+  end
 end
