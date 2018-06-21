e72114b101a9503b23e12de1a83342ca24dc42a4
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-06-21 19:21:05 +0900

    リロードせずにいいねを反映するように変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    current_user.follow(Post.find(params[:follow]))
-    redirect_back(fallback_location: root_path)
+    @size = params[:size]
+    @post = Post.find(params[:follow])
+    current_user.follow(@post)
   end
 
   def destroy
-    current_user.stop_following(Post.find(params[:id]))
-    redirect_back(fallback_location: root_path)
+    @size = params[:size]
+    @post = Post.find(params[:id])
+    current_user.stop_following(@post)
   end
 end
