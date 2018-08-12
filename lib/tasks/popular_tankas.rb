ffca9e7b3ef82e985ac0bf9f24dc8890a90b4756
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-08-12 12:58:53 +0900

    スクリプトの修正

--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -1,4 +1,4 @@
-popular_tankas = Follow.where(["followable_type = :type", {type: 'Post'}]).group('follows.followable_id').count('follows.followable_id')
-popular_posts = popular_posts.select{|k, v| v >= 10}
-popular_posts_ids = Hash[popular_posts.sort_by{ |_, v| -v }].keys
-puts popular_posts_ids
+favorites_count = Follow.where(["followable_type = :type", {type: 'Post'}]).group('follows.followable_id').count('follows.followable_id')
+popular_tankas = favorites_count.select{|k, v| v >= 10}
+sorted_popular_tankas = Hash[popular_tankas.sort{ |(k1, v1), (k2, v2)| v2 <=> v1 }]
+puts sorted_popular_tankas
