e8d2e3039c5e8bf9e4642a81d11c88d5dbdce8c2
安達広介 <fuyu@adachihirosuke-no-MacBook-Air.local>
2018-09-13 18:42:18 +0900

    Twitterシェア機能で"#"が表示されない不具合を修正

---
+++ lib/tasks/duplicate_like.rb
@@ -0,0 +1,12 @@
+users = User.all.map{|t| t.id}
+is_duplicate_like = true
+users.each do |id|
+  likes_count = Follow.where(followable_type: "Post", follower_id: id).group("follows.followable_id").count
+  duplicate_like = likes_count.select{|k, v| v >= 2}
+  if duplicate_like.present?
+    is_duplicate_like = false
+    puts id
+    puts duplicate_like
+  end
+end
+puts 'OK' if is_duplicate_like
