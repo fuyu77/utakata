41bb7395215766b701f647a2a75c131e1201dbb3
fuyu77 <kiwi.vc@gmail.com>
2018-11-20 14:39:52 +0900

    短歌の字数制限を緩和

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   acts_as_followable
   belongs_to :user, inverse_of: :posts
   validates :user_id, presence: true
-  validates :tanka, presence: true, uniqueness: true, length: { minimum: 5, maximum: 350 }
+  validates :tanka, presence: true, uniqueness: true, length: { minimum: 5, maximum: 1000 }
 
   def self.search(search)
     if search
