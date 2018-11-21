aa7343466c9156998ce2f1a1da2bf19b0d588ae8
fuyu77 <kiwi.vc@gmail.com>
2018-11-22 05:16:19 +0900

    リファクタ

--- app/javascript/controllers/post_controller.js
+++ app/javascript/controllers/post_controller.js
@@ -5,25 +5,33 @@ export default class extends Controller {
   static targets = [ "input", "preview" ]
 
   initialize() {
-    this.previewTarget.innerHTML = this.input
+    this.preview = this.input
   }
 
-  preview() {
+  previewPost() {
     const value = striptags(this.input, ["ruby", "rt", "rp"])
-    this.previewTarget.innerHTML = value
+    this.preview = value
   }
 
   space() {
     const oldText = this.input
     const cursorPosition = this.inputTarget.selectionStart
     const newText = `${oldText.slice(0, cursorPosition)}　${oldText.substr(cursorPosition)}`
-    this.previewTarget.innerHTML = newText
-    this.inputTarget.value = newText
+    this.preview = newText
+    this.input = newText
     this.inputTarget.focus()
     this.inputTarget.setSelectionRange(cursorPosition + 1, cursorPosition + 1)
   }
 
   get input() {
     return this.inputTarget.value
+  }
+
+  set input(value) {
+    return this.inputTarget.value = value
+  }
+  
+  set preview(html) {
+    this.previewTarget.innerHTML = html
   }
 }
