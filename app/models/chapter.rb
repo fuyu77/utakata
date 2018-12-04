f91848fbc0d21041342be84bf4016022c50fcf1f
fuyu77 <kiwi.vc@gmail.com>
2018-12-04 23:11:58 +0900

    Chapterモデルの設定

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Chapter < ApplicationRecord
+  acts_as_followable
+  acts_as_follower
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 1000 }
 end
