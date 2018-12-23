568ef6007ac128eaeaf66c82bbb8efcb76a96486
fuyu77 <kiwi.vc@gmail.com>
2018-12-23 13:56:16 +0900

    リファクタ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create destroy search_mine]
+  before_action :authenticate_user!, only: %i[new create update destroy my_search]
 
   def new
     @post = current_user.posts.build
