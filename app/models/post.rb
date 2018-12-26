f585b3b8687fb62a9dd7d9c68183fe54de857f49
fuyu77 <kiwi.vc@gmail.com>
2018-12-26 20:59:53 +0900

    リファクタ

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@
 class Post < ApplicationRecord
   acts_as_followable
   belongs_to :user
-  validates :user_id, presence: true
   validates :tanka, presence: true, uniqueness: true, length: { minimum: 5, maximum: 1000 }
 
   def self.search(search)
