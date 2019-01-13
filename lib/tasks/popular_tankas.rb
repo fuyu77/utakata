cc87bc002d75821ebd8eacffe0f61a3aa1cffda3
fuyu77 <kiwi.vc@gmail.com>
2019-01-13 18:17:34 +0900

    リファクタ

--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -3,4 +3,4 @@
 favorites_count = Follow.where(['followable_type = :type', { type: 'Post' }]).group('follows.followable_id').count('follows.followable_id')
 popular_tankas = favorites_count.select { |_, v| v >= 10 }
 sorted_popular_tankas = Hash[popular_tankas.sort { |(_, v1), (_, v2)| v2 <=> v1 }]
-puts sorted_popular_tankas
+p sorted_popular_tankas
