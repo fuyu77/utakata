a4fca6cf10033ffa02a38226b74582ebce3a3bce
fuyu77 <kiwi.vc@gmail.com>
2019-01-13 19:37:01 +0900

    不要なスクリプトを削除

--- lib/tasks/add_published_at.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Post.all.each do |p|
-  p.published_at = p.created_at
-  p.save!
-end
--- lib/tasks/deleted_users.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-users = User.all.pluck(:id)
-last_user = User.last.id
-original_users = [*(1..last_user)]
-puts(original_users.reject { |user| users.include?(user) })
