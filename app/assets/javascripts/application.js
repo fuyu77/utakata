536fdb014f5cf2ff18cf6cb8070d8382b2d50e02
fuyu77 <kiwi.vc@gmail.com>
2019-03-04 00:20:33 +0900

    調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
 //= require bootstrap-filestyle.min
 //= require popper
 //= require bootstrap-sprockets
