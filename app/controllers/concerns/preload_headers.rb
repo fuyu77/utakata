46a4e12e3f3528274f47bedf7fa37c895869524e
fuyu77 <kiwi.vc@gmail.com>
2019-03-03 19:54:30 +0900

    サイトを高速化

---
+++ app/controllers/concerns/preload_headers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module PreloadHeaders
+  extend ActiveSupport::Concern
+  included do
+    after_action :set_preload_headers
+  end
+
+  protected
+
+  def set_preload_headers
+    return if !request.format.html? || request.xhr? || request.headers['X-XHR-Referer'].present? || request.env['HTTP_TURBOLINKS_REFERRER']
+
+    response.headers['Link'] = preload_header_assets.map do |asset|
+      "<#{view_context.asset_path(asset[:path])}>; rel=preload; as=#{asset[:as]}"
+    end
+  end
+end
