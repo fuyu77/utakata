49a12a05c8958dd0644f0f8b96a050c9b18e4ee3
fuyu77 <kiwi.vc@gmail.com>
2020-01-04 00:06:41 +0900

    sprocketsからwebpackerに移行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include ::PreloadHeaders
+  # include ::PreloadHeaders
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :ensure_domain
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import "../stylesheets/application.scss"
 import Rails from "rails-ujs"
 import Turbolinks from "turbolinks"
 import { Application } from "stimulus"
