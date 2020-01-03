dfe7c6eaba8d2ec200cf80611ca6ef39e94670da
fuyu77 <kiwi.vc@gmail.com>
2020-01-04 01:08:02 +0900

    調整

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Utakata
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
-    config.assets.enabled = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
