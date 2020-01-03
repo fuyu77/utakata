713816fbed5132b0c371bf511ca61ea840eb6ed7
fuyu77 <kiwi.vc@gmail.com>
2020-01-03 20:23:43 +0900

    fontawesomeをwebpackerに移行

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import Turbolinks from "turbolinks"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import toastr from "toastr"
+import "@fortawesome/fontawesome-free/js/all"
 
 global.toastr = toastr
 Rails.start()
