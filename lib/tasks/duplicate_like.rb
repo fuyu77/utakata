e3061de7722c4a39e8f5d126672bfec3c4363922
fuyu77 <kiwi.vc@gmail.com>
2020-03-14 23:26:27 +0900

    スクリプト改修

--- lib/tasks/duplicate_like.rb
+++ lib/tasks/duplicate_like.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 users = User.all.map(&:id)
-is_duplicate_like = true
+is_duplicate_like = false
 users.each do |id|
   likes_count = Follow.where(followable_type: 'Post', follower_id: id).group('follows.followable_id').count
   duplicate_like = likes_count.select { |_, v| v >= 2 }
   next unless duplicate_like.present?
 
-  is_duplicate_like = false
+  is_duplicate_like = true
   puts id
   puts duplicate_like
 end
-puts is_duplicate_like
+puts is_duplicate_like ? '重複あり' : '重複なし'
