0a4e222f8b23bdb7470e4d15fafe275a54ff3e50
fuyu77 <kiwi.vc@gmail.com>
2020-03-22 21:33:01 +0900

    スクリプトを追加

---
+++ lib/tasks/forgotten_like.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+exluded_post_ids = Follow.where(followable_type: 'Post', follower_id: 5).pluck(:followable_id)
+exluded_post_ids += Post.where(user_id: 5).pluck(:id)
+posts_with_like_count = Follow.where(followable_type: 'Post').where.not(followable_id: exluded_post_ids).group('follows.followable_id').count('follows.followable_id')
+posts = posts_with_like_count.select { |_, v| v >= 2 }
+p posts
+p posts.length
--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-favorites_count = Follow.where(['followable_type = :type', { type: 'Post' }]).group('follows.followable_id').count('follows.followable_id')
+favorites_count = Follow.where(followable_type: 'Post').group('follows.followable_id').count('follows.followable_id')
 popular_tankas = favorites_count.select { |_, v| v >= 10 }
 sorted_popular_tankas = Hash[popular_tankas.sort { |(_, v1), (_, v2)| v2 <=> v1 }]
 p sorted_popular_tankas
