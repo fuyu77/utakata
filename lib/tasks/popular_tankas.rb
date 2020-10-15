60fb9a500461e016c61a4bc14ab5449da833e7b0
fuyu77 <kiwi.vc@gmail.com>
2020-10-16 00:13:47 +0900

    スクリプト修正

--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-favorites_count = Follow.where(followable_type: 'Post').group('follows.followable_id').count('follows.followable_id')
+favorites_count = Follow.joins('INNER JOIN posts ON posts.id = follows.followable_id')
+                        .where(follows: { followable_type: 'Post' })
+                        .where.not(posts: { user_id: [5, 1046] })
+                        .group('follows.followable_id')
+                        .count('follows.followable_id')
+
 popular_tankas = favorites_count.select { |_, v| v >= 10 }
 sorted_popular_tankas = Hash[popular_tankas.sort { |(_, v1), (_, v2)| v2 <=> v1 }]
 p sorted_popular_tankas
