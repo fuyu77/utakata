985085938cfcf6ffd633cfde44aae7557a09cd08
fuyu77 <kiwi.vc@gmail.com>
2020-11-03 08:08:48 +0900

    webpack用のESLint loaderは使わない

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,13 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-const webpack = require('webpack')
-const ESLintPlugin = require('eslint-webpack-plugin')
 
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    eslint_webpack_plugin: [new ESLintPlugin()]
-  })
-)
 module.exports = environment.toWebpackConfig()
