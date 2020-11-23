a1d83c214b147f3590267a4bbe2c050c8a887533
fuyu77 <kiwi.vc@gmail.com>
2020-11-23 16:59:28 +0900

    スクリプト変更

--- lib/tasks/forgotten_likes.rb
+++ lib/tasks/forgotten_likes.rb
@@ -6,6 +6,6 @@ posts_with_like_count = Follow.where(followable_type: 'Post')
                               .where.not(followable_id: exluded_post_ids)
                               .group('follows.followable_id')
                               .count('follows.followable_id')
-posts = posts_with_like_count.select { |_, v| v >= 4 }
+posts = posts_with_like_count.select { |_, v| v >= 9 }
 p posts
 p posts.length
