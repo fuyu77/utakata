2f0d3826c74c1f4d2afbea11f88fb61fd16385d2
fuyu77 <kiwi.vc@gmail.com>
2021-03-06 21:25:46 +0900

    RubCop修正

--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -6,6 +6,6 @@ favorites_count = Follow.joins('INNER JOIN posts ON posts.id = follows.followabl
                         .group('follows.followable_id')
                         .count('follows.followable_id')
 popular_tankas = favorites_count.select { |_, v| v >= 10 }
-sorted_popular_tankas = Hash[popular_tankas.sort { |(_, v1), (_, v2)| v2 <=> v1 }]
+sorted_popular_tankas = popular_tankas.sort { |(_, v1), (_, v2)| v2 <=> v1 }.to_h
 p sorted_popular_tankas
 p sorted_popular_tankas.length
