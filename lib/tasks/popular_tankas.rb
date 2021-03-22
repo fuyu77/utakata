8bef327ec6c5029308bd445c006de2f1a7390405
fuyu77 <kiwi.vc@gmail.com>
2021-03-22 16:56:53 +0900

    スクリプト置換

--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -1,11 +1,37 @@
 # frozen_string_literal: true
 
-favorites_count = Follow.joins('INNER JOIN posts ON posts.id = follows.followable_id')
+popular_records = Follow.select('
+                          follows.followable_id,
+                          MIN(posts.user_id) as user_id,
+                          COUNT(follows.id) as favorites_count
+                        ')
+                        .joins('INNER JOIN posts ON posts.id = follows.followable_id')
                         .where(follows: { followable_type: 'Post' })
                         .where.not(posts: { user_id: [5, 1046] })
                         .group('follows.followable_id')
-                        .count('follows.followable_id')
-popular_tankas = favorites_count.select { |_, v| v >= 10 }
-sorted_popular_tankas = popular_tankas.sort { |(_, v1), (_, v2)| v2 <=> v1 }.to_h
-p sorted_popular_tankas
-p sorted_popular_tankas.length
+                        .having('COUNT(follows.id) >= 10')
+
+popular_posts = popular_records.each_with_object([]) do |record, result|
+  next if record.user_id == 857 && record.favorites_count <= 10
+
+  result << {
+    id: record.followable_id,
+    user_id: record.user_id,
+    favorites_count: record.favorites_count
+  }
+end
+
+p(popular_posts.map { |post| post[:id] })
+p popular_posts.length
+
+posts_count_per_user = popular_posts.each_with_object({}) do |post, result|
+  if result[post[:user_id]]
+    result[post[:user_id]] += 1
+  else
+    result[post[:user_id]] = 1
+  end
+end
+
+sorted_posts_count_per_user = posts_count_per_user.sort { |(_, v1), (_, v2)| v2 <=> v1 }.to_h
+
+p sorted_posts_count_per_user
