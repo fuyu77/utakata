7f656108ea400147c97048b81945accf4f5b0517
fuyu77 <kiwi.vc@gmail.com>
2021-06-18 23:08:53 +0900

    ロジック調整

--- lib/tasks/popular_tankas.rb
+++ lib/tasks/popular_tankas.rb
@@ -12,8 +12,9 @@ popular_records = Follow.select('
                         .having('COUNT(follows.id) >= 10')
 
 popular_posts = popular_records.each_with_object([]) do |record, result|
-  next if record.user_id == 857 && record.favorites_count <= 13
-  next if record.user_id.in?([440, 2146]) && record.favorites_count <= 13
+  next if record.user_id == 857 && record.favorites_count < 14
+  next if record.user_id.in?([440, 2146]) && record.favorites_count < 14
+  next if record.user_id == 1476 && record.favorites_count < 11
 
   result << {
     id: record.followable_id,
