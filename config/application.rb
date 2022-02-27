ce9fb3c2402cc9a5599fc203eabd5de5de509180
fuyu77 <kiwi.vc@gmail.com>
2022-02-27 22:14:17 +0900

    Update Rails

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,8 @@ Bundler.require(*Rails.groups)
 module Utakata
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
     config.time_zone = 'Tokyo'
-    config.assets.enabled = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
