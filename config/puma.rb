4bb58aa68c9ae348571f7b3dc0236abc96008171
fuyu77 <kiwi.vc@gmail.com>
2022-04-29 12:39:44 +0900

    Fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-workers Integer(ENV.fetch('WEB_CONCURRENCY', nil) || 2)
-threads_count = Integer(ENV.fetch('RAILS_MAX_THREADS', nil) || 5)
+workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+threads_count = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV.fetch('PORT', nil)     || 3000
-environment ENV.fetch('RACK_ENV', nil) || 'development'
+port        ENV.fetch('PORT', 3000)
+environment ENV.fetch('RACK_ENV', 'development')
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
