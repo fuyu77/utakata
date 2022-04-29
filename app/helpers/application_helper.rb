005e3831ac50083800df81da273767cb85ffef62
fuyu77 <kiwi.vc@gmail.com>
2022-04-29 11:54:43 +0900

    Fix RuboCop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,22 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def title
-    title = 'Utakata - 短歌投稿サイト'
-    title = "#{@title} - 短歌投稿サイトUtakata" if @title.present?
-    title
+  def title(title:)
+    return 'Utakata - 短歌投稿サイト' if title.blank?
+
+    "#{title} - 短歌投稿サイトUtakata"
   end
 
-  def meta_description
-    description = ''
-    if current_page?(root_path)
-      description = '短歌を投稿・共有できるサイトです。あなたの作品を縦書きで美しく表現します。'
-    elsif @description.present?
-      description = @description
-    end
+  def meta_description(description:)
+    return '短歌を投稿・共有できるサイトです。あなたの作品を縦書きで美しく表現します。' if current_page?(root_path)
+
     description
   end
 
-  def og_image_url
-    url = ''
-    url = "https:#{@image_url}" if @image_url.present?
-    url
+  def og_image_url(image_url:)
+    return if image_url.blank?
+
+    "https:#{image_url}"
   end
 
   def notifications_count
