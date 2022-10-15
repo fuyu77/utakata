fc0351c568fb344f83f8779511b016189c09c68f
fuyu77 <kiwi.vc@gmail.com>
2022-10-15 15:24:49 +0900

    FIx

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
-
 import '../stylesheets/application.scss'
 import '@hotwired/turbo-rails'
 import { Application } from '@hotwired/stimulus'
