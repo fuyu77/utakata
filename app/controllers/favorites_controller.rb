5f0589294ac294f7c7a5b48f21634158f7c71d56
fuyu77 <kiwi.vc@gmail.com>
2022-10-23 21:02:59 +0900

    Refactor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,24 +2,24 @@
 
 class FavoritesController < ApplicationController
   def create
-    @post = Post.find(params[:follow])
-    return if current_user.following?(@post)
+    post = Post.find(params[:follow])
+    return if current_user.following?(post)
 
-    current_user.follow(@post)
+    current_user.follow(post)
     render turbo_stream: turbo_stream.replace(
-      @post,
+      post,
       partial: "favorites/unlike_#{params[:size]}",
-      locals: { post: @post }
+      locals: { post: }
     )
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    current_user.stop_following(@post)
+    post = Post.find(params[:id])
+    current_user.stop_following(post)
     render turbo_stream: turbo_stream.replace(
-      @post,
+      post,
       partial: "favorites/like_#{params[:size]}",
-      locals: { post: @post }
+      locals: { post: }
     )
   end
 end
