d6dfaf033641a41d14f3797c8bbbb63eedfdf1b2
fuyu77 <kiwi.vc@gmail.com>
2022-11-03 18:55:40 +0900

    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def title(title:)
+  def title(title)
     return 'Utakata - 短歌投稿サイト' if title.blank?
 
     "#{title} - 短歌投稿サイトUtakata"
   end
 
-  def meta_description(description:)
+  def meta_description(description)
     return '短歌を投稿・共有できるサイトです。あなたの作品を縦書きで美しく表現します。' if current_page?(root_path)
 
     description
   end
 
-  def og_image_url(image_url:)
+  def og_image_url(image_url)
     return if image_url.blank?
 
     "https:#{image_url}"
