c0be98695afaa05ad2f7f204ab5791f842a7acc3
fuyu77 <kiwi.vc@gmail.com>
2022-11-03 14:13:20 +0900

    Remove comment

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,7 +4,6 @@ class Follow < ApplicationRecord
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
 
-  # NOTE: Follows belong to the "followable" and "follower" interface
   belongs_to :followable, polymorphic: true
   belongs_to :follower,   polymorphic: true
 end
