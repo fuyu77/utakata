19db40858df2b0b98e36cc41a9b8d1f83b79b582
fuyu77 <kiwi.vc@gmail.com>
2022-11-03 10:00:28 +0900

    Fix

--- lib/tasks/duplicate_likes.rb
+++ lib/tasks/duplicate_likes.rb
@@ -3,8 +3,8 @@
 user_ids = User.pluck(:id)
 is_duplicate_like = false
 user_ids.each do |id|
-  likes_count = Follow.where(followable_type: 'Post', follower_id: id).group('follows.followable_id').count
-  duplicate_like = likes_count.select { |_, v| v >= 2 }
+  favorites_count = Follow.where(followable_type: 'Post', follower_id: id).group('follows.followable_id').count
+  duplicate_like = favorites_count.select { |_, v| v >= 2 }
   next if duplicate_like.blank?
 
   is_duplicate_like = true
