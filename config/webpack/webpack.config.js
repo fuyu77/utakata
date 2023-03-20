5e9d15be878fd8602bce94e9eede5853e8b75da2
fuyu77 <kiwi.vc@gmail.com>
2023-03-20 15:17:49 +0900

    refactor: resolve ESLint errors

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,52 +1,52 @@
-const path = require('path')
-const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts')
-const CompressionPlugin = require('compression-webpack-plugin')
-const { globSync } = require('glob')
-const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const CompressionPlugin = require('compression-webpack-plugin');
+const { globSync } = require('glob');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
 
 const PATHS = {
-  src: path.join(__dirname, '../../app/views')
-}
+  src: path.join(__dirname, '../../app/views'),
+};
 
 module.exports = {
   mode: 'production',
   entry: {
-    application: ['./app/assets/javascripts/application.js']
+    application: ['./app/assets/javascripts/application.js'],
   },
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.s?[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, '..', '..', 'app/assets/builds')
+    path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
+      maxChunks: 1,
     }),
     new RemoveEmptyScriptsPlugin(),
     new MiniCssExtractPlugin(),
     new PurgeCSSPlugin({
       paths: globSync(`${PATHS.src}/**/*`, { nodir: true }),
-      safelist: ['tate', 'user_avatar']
+      safelist: ['tate', 'user_avatar'],
     }),
     new CompressionPlugin({
-      test: /\.(js)|(s?[ac]ss)$/i
-    })
+      test: /\.(js)|(s?[ac]ss)$/i,
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.scss', '.css']
-  }
-}
+    extensions: ['.js', '.jsx', '.scss', '.css'],
+  },
+};
