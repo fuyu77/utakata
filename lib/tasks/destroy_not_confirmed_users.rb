37688c6f90a6b4b6d98b73062caa04078214f4d2
fuyu77 <kiwi.vc@gmail.com>
2023-09-12 09:00:30 +0900

    fix: fix for RuboCop

--- lib/tasks/destroy_not_confirmed_users.rb
+++ lib/tasks/destroy_not_confirmed_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-User.where(confirmed_at: nil).where('created_at < ?', 1.day.ago).each do |user|
+User.where(confirmed_at: nil).where('created_at < ?', 1.day.ago).find_each do |user|
   Rails.logger.debug user.id
   Rails.logger.debug user.email
   Rails.logger.debug user.name
