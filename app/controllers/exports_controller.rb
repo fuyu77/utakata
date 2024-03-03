e6b021a640ed1fd4744614c8552c84bbceb513d9
fuyu77 <kiwi.vc@gmail.com>
2024-03-03 21:19:49 +0900

    fix: fix CSV generation

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,9 +1,11 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 class ExportsController < ApplicationController
   before_action :authenticate_user!, only: %i[index]
 
   def index
+    return if request.format.to_s == 'text/html'
+
     posts = current_user.posts.includes(:followings).order('published_at DESC')
     csv_data = CSV.generate("\uFEFF") do |csv|
       headers = %w[短歌 投稿日時 いいね数]
@@ -18,7 +20,6 @@ class ExportsController < ApplicationController
       end
     end
     respond_to do |format|
-      format.html
       format.csv { send_data csv_data, filename: '投稿短歌.csv' }
     end
   end
