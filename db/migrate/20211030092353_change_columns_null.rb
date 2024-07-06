b608c17e5304bbd11029b5b412f950c045e820c6
fuyu77 <kiwi.vc@gmail.com>
2024-07-06 14:40:06 +0900

    fix: turn off RuboCop warning

--- db/migrate/20211030092353_change_columns_null.rb
+++ db/migrate/20211030092353_change_columns_null.rb
@@ -2,7 +2,7 @@
 
 class ChangeColumnsNull < ActiveRecord::Migration[6.1]
   def change
-    change_column_null :posts, :user_id, false
+    change_column_null :posts, :user_id, false # rubocop:disable Rails/BulkChangeTable
     change_column_null :posts, :tanka, false, ''
     change_column_null :posts, :published_at, false
   end
