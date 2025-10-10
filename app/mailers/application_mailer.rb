fbae192aa17c8e3feecb57411efdc3ce862db976
fuyu77 <kiwi.vc@gmail.com>
2025-10-10 13:39:05 +0900

    feat: update default email sender in ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com',
+  default from: 'utakatanka@gmail.com',
           reply_to: 'utakatanka@gmail.com'
   layout 'mailer'
 end
