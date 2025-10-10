7fdc411da940b336866cd869283b1a64962feaad
fuyu77 <kiwi.vc@gmail.com>
2025-10-10 15:23:52 +0900

    feat: set display name for mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'utakatanka@gmail.com',
+  default from: '短歌投稿サイトUtakata <utakatanka@gmail.com>',
           reply_to: 'utakatanka@gmail.com'
   layout 'mailer'
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,7 +29,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'utakatanka@gmail.com'
+  config.mailer_sender = '短歌投稿サイトUtakata <utakatanka@gmail.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
