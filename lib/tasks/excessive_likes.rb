f0e81ba21048033938bb6fa287197c0b2945947b
fuyu77 <kiwi.vc@gmail.com>
2025-10-27 09:30:15 +0900

    refactor: remove unused like-related tasks

--- lib/tasks/excessive_likes.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-post_ids = Follow.where(followable_type: 'Post', follower_id: 5).pluck(:followable_id)
-posts_with_favorites_count = Follow.where(followable_type: 'Post', followable_id: post_ids)
-                                   .group('follows.followable_id')
-                                   .count('follows.followable_id')
-posts = posts_with_favorites_count.select { |_, v| v <= 9 }
-Rails.logger.debug posts.keys.join(',')
-Rails.logger.debug posts.length
