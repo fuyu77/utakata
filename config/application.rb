9e9a6466f2d7cac212f783517cc319df46523239
fuyu77 <kiwi.vc@gmail.com>
2025-10-31 09:27:40 +0900

    refactor: enable YJIT in application configuration

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ end
 class Utakata::Application < Rails::Application
   config.load_defaults 8.1
   config.time_zone = 'Tokyo'
+  config.yjit = true
 
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
 end
