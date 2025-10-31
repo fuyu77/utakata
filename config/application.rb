43a18806472151dda9784f7563ac15aad8d9f611
fuyu77 <kiwi.vc@gmail.com>
2025-10-31 09:24:27 +0900

    refactor: application.rb by removing commented-out configuration lines for clarity

--- config/application.rb
+++ config/application.rb
@@ -12,16 +12,8 @@ module Utakata
 end
 
 class Utakata::Application < Rails::Application
-  # Initialize configuration defaults for originally generated Rails version.
   config.load_defaults 8.1
-
-  # Configuration for the application, engines, and railties goes here.
-  #
-  # These settings can be overridden in specific environments using the files
-  # in config/environments, which are processed later.
-  #
   config.time_zone = 'Tokyo'
-  # config.eager_load_paths << Rails.root.join("extras")
 
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
 end
