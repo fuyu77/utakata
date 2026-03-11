27bc3eaa6291e11dcb997ea12598f2aa24b60988
fuyu77 <kiwi.vc@gmail.com>
2026-03-11 15:09:11 +0900

    RuboCopのStyle/OneClassPerFileに対応

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 require_relative 'boot'
+require_relative '../lib/utakata'
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-module Utakata
-end
 
 class Utakata::Application < Rails::Application
   config.load_defaults 8.1
