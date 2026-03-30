873e1a06e9ff71de95edc00acab79cbc5fdc01da
fuyu77 <kiwi.vc@gmail.com>
2026-03-30 13:25:14 +0900

    ログレベルを変更

--- lib/tasks/destroy_not_confirmed_users.rb
+++ lib/tasks/destroy_not_confirmed_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 User.where(confirmed_at: nil).where(created_at: ...1.day.ago).find_each do |user|
-  Rails.logger.debug user.id
-  Rails.logger.debug user.email
-  Rails.logger.debug user.name
+  Rails.logger.info user.id
+  Rails.logger.info user.email
+  Rails.logger.info user.name
   user.destroy!
 end
