4f1bfbe963b232615385931c4ea37a954b31e6b4
fuyu77 <kiwi.vc@gmail.com>
2026-04-25 23:19:13 +0900

    Postからpopular_postsへの関連付けを追加

--- app/controllers/posts/popular_controller.rb
+++ app/controllers/posts/popular_controller.rb
@@ -3,7 +3,7 @@
 class Posts::PopularController < ApplicationController
   def index
     @posts = Post.includes(:user, :followings)
-                 .joins('INNER JOIN popular_posts ON popular_posts.post_id = posts.id')
+                 .joins(:popular_posts)
                  .order('popular_posts.position')
                  .page(params[:page])
   end
