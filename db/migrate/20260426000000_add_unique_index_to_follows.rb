11a812d5d4fc91756f309cdf373b21e52b47db74
fuyu77 <kiwi.vc@gmail.com>
2026-04-26 10:45:02 +0900

    Remove redundant SQL for deleting duplicate follows before adding unique index

--- db/migrate/20260426000000_add_unique_index_to_follows.rb
+++ db/migrate/20260426000000_add_unique_index_to_follows.rb
@@ -2,23 +2,6 @@
 
 class AddUniqueIndexToFollows < ActiveRecord::Migration[8.1]
   def up
-    execute <<~SQL.squish
-      DELETE FROM follows
-      WHERE id IN (
-        SELECT id
-        FROM (
-          SELECT
-            id,
-            ROW_NUMBER() OVER (
-              PARTITION BY follower_id, follower_type, followable_id, followable_type
-              ORDER BY id
-            ) AS row_number
-          FROM follows
-        ) duplicate_follows
-        WHERE duplicate_follows.row_number > 1
-      )
-    SQL
-
     add_index :follows,
               %i[follower_id follower_type followable_id followable_type],
               unique: true,
