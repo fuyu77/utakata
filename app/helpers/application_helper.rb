a0f9d018eb3d56704e36b05043d90d200fbb6e55
fuyu77 <kiwi.vc@gmail.com>
2026-05-03 19:27:22 +0900

    ユーザーアイコン表示をヘルパー化する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  AVATAR_IMAGE_DISPLAY_SIZES = {
+    original: 75,
+    medium: 35,
+    small: 20
+  }.freeze
+
   def title(title)
     return 'Utakata - 短歌投稿サイト' if title.blank?
 
@@ -22,5 +28,11 @@ module ApplicationHelper
   def notifications_count
     notifications_count = Follow.where(user_id: current_user.id, read: false).count
     notifications_count.zero? ? nil : notifications_count
+  end
+
+  def avatar_image_tag(user, size, options = {})
+    display_size = AVATAR_IMAGE_DISPLAY_SIZES.fetch(size)
+
+    image_tag user.avatar.url(size), options.reverse_merge(width: display_size, height: display_size)
   end
 end
