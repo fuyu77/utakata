5c4bc49890466d31686d95a6637f11059cebde73
fuyu77 <kiwi.vc@gmail.com>
2026-08-15 22:10:48 +0900

    Middlewareテストの準備を各ケース内で完結させる

--- test/middlewares/canonical_host_redirect_test.rb
+++ test/middlewares/canonical_host_redirect_test.rb
@@ -3,28 +3,34 @@
 require 'test_helper'
 
 describe CanonicalHostRedirect do
-  before do
-    inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
-    @middleware = CanonicalHostRedirect.new(inner_app)
-  end
+  describe '#call' do
+    it 'Heroku標準ドメインへのアクセスをパスとクエリを維持して正規ドメインへ恒久的にリダイレクトする' do
+      inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
+      middleware = CanonicalHostRedirect.new(inner_app)
 
-  it 'Heroku標準ドメインへのアクセスをパスとクエリを維持して正規ドメインへ恒久的にリダイレクトする' do
-    status, headers = @middleware.call(Rack::MockRequest.env_for('https://utakatanka.herokuapp.com/posts/1?ref=share'))
+      status, headers = middleware.call(Rack::MockRequest.env_for('https://utakatanka.herokuapp.com/posts/1?ref=share'))
 
-    assert_equal 301, status
-    assert_equal 'https://utakatanka.jp/posts/1?ref=share', headers.fetch('location')
-  end
+      assert_equal 301, status
+      assert_equal 'https://utakatanka.jp/posts/1?ref=share', headers.fetch('location')
+    end
 
-  it '正規ドメインへのアクセスはそのまま処理する' do
-    status, headers = @middleware.call(Rack::MockRequest.env_for('https://utakatanka.jp/posts/1?ref=share'))
+    it '正規ドメインへのアクセスはそのまま処理する' do
+      inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
+      middleware = CanonicalHostRedirect.new(inner_app)
 
-    assert_equal 200, status
-    assert_equal 'application/test', headers.fetch('content-type')
-  end
+      status, headers = middleware.call(Rack::MockRequest.env_for('https://utakatanka.jp/posts/1?ref=share'))
 
-  it 'Heroku以外のドメインへのアクセスはそのまま処理する' do
-    status, = @middleware.call(Rack::MockRequest.env_for('https://example.com/posts/1'))
+      assert_equal 200, status
+      assert_equal 'application/test', headers.fetch('content-type')
+    end
 
-    assert_equal 200, status
+    it 'Heroku以外のドメインへのアクセスはそのまま処理する' do
+      inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
+      middleware = CanonicalHostRedirect.new(inner_app)
+
+      status, = middleware.call(Rack::MockRequest.env_for('https://example.com/posts/1'))
+
+      assert_equal 200, status
+    end
   end
 end
