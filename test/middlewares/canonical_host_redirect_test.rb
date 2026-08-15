0d5d0fa7599e181d250e8711d7d8e2cf86161b8d
fuyu77 <kiwi.vc@gmail.com>
2026-08-15 22:15:47 +0900

    Herokuドメイン判定にHTTP_HOSTを使用する

--- test/middlewares/canonical_host_redirect_test.rb
+++ test/middlewares/canonical_host_redirect_test.rb
@@ -8,7 +8,12 @@ describe CanonicalHostRedirect do
       inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
       middleware = CanonicalHostRedirect.new(inner_app)
 
-      status, headers = middleware.call(Rack::MockRequest.env_for('https://utakatanka.herokuapp.com/posts/1?ref=share'))
+      env = Rack::MockRequest.env_for(
+        '/posts/1?ref=share',
+        'HTTP_HOST' => 'utakatanka.herokuapp.com'
+      )
+
+      status, headers = middleware.call(env)
 
       assert_equal 301, status
       assert_equal 'https://utakatanka.jp/posts/1?ref=share', headers.fetch('location')
@@ -18,17 +23,55 @@ describe CanonicalHostRedirect do
       inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
       middleware = CanonicalHostRedirect.new(inner_app)
 
-      status, headers = middleware.call(Rack::MockRequest.env_for('https://utakatanka.jp/posts/1?ref=share'))
+      env = Rack::MockRequest.env_for(
+        '/posts/1?ref=share',
+        'HTTP_HOST' => 'utakatanka.jp'
+      )
+
+      status, headers = middleware.call(env)
 
       assert_equal 200, status
       assert_equal 'application/test', headers.fetch('content-type')
+    end
+
+    it '転送ヘッダーが正規ドメインでもHTTP_HOSTがHeroku標準ドメインならリダイレクトする' do
+      inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
+      middleware = CanonicalHostRedirect.new(inner_app)
+      env = Rack::MockRequest.env_for(
+        '/posts/1',
+        'HTTP_HOST' => 'utakatanka.herokuapp.com',
+        'HTTP_FORWARDED' => 'host=utakatanka.jp',
+        'HTTP_X_FORWARDED_HOST' => 'utakatanka.jp'
+      )
+
+      status, headers = middleware.call(env)
+
+      assert_equal 301, status
+      assert_equal 'https://utakatanka.jp/posts/1', headers.fetch('location')
+    end
+
+    it '転送ヘッダーがHeroku標準ドメインでもHTTP_HOSTが正規ドメインならリダイレクトしない' do
+      inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
+      middleware = CanonicalHostRedirect.new(inner_app)
+      env = Rack::MockRequest.env_for(
+        '/posts/1',
+        'HTTP_HOST' => 'utakatanka.jp',
+        'HTTP_FORWARDED' => 'host=utakatanka.herokuapp.com',
+        'HTTP_X_FORWARDED_HOST' => 'utakatanka.herokuapp.com'
+      )
+
+      status, = middleware.call(env)
+
+      assert_equal 200, status
     end
 
     it 'Heroku以外のドメインへのアクセスはそのまま処理する' do
       inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
       middleware = CanonicalHostRedirect.new(inner_app)
 
-      status, = middleware.call(Rack::MockRequest.env_for('https://example.com/posts/1'))
+      env = Rack::MockRequest.env_for('/posts/1', 'HTTP_HOST' => 'example.com')
+
+      status, = middleware.call(env)
 
       assert_equal 200, status
     end
