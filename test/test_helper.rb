f8b2b05348adf93a4d48cdf2d7025f6d1cd62ed1
fuyu77 <kiwi.vc@gmail.com>
2026-08-15 18:24:40 +0900

    不要なコメントを削除

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,5 @@ require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # test/fixtures/*.ymlにあるすべてのフィクスチャをアルファベット順に読み込む。
   fixtures :all
-
-  # すべてのテストで使用するヘルパーメソッドはここに追加する。
 end
