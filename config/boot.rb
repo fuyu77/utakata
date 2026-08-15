9dbc8f35a3d5e29c5295a79f968d8054a2024009
fuyu77 <kiwi.vc@gmail.com>
2026-08-16 08:42:11 +0900

    テスト実行時のJavaScriptビルドを省略する

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['SKIP_JS_BUILD'] ||= 'true' if ARGV.first&.match?(/\Atest(?::|\z)/)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
