1730c94c808b35a7eb2785eec1f7a985749c585b
fuyu77 <kiwi.vc@gmail.com>
2026-08-15 22:08:18 +0900

    Middlewareテストからletを取り除く

--- test/middlewares/canonical_host_redirect_test.rb
+++ test/middlewares/canonical_host_redirect_test.rb
@@ -3,25 +3,27 @@
 require 'test_helper'
 
 describe CanonicalHostRedirect do
-  let(:inner_app) { ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] } }
-  let(:middleware) { CanonicalHostRedirect.new(inner_app) }
+  before do
+    inner_app = ->(_env) { [200, { 'content-type' => 'application/test' }, ['response']] }
+    @middleware = CanonicalHostRedirect.new(inner_app)
+  end
 
   it 'Heroku標準ドメインへのアクセスをパスとクエリを維持して正規ドメインへ恒久的にリダイレクトする' do
-    status, headers = middleware.call(Rack::MockRequest.env_for('https://utakatanka.herokuapp.com/posts/1?ref=share'))
+    status, headers = @middleware.call(Rack::MockRequest.env_for('https://utakatanka.herokuapp.com/posts/1?ref=share'))
 
     assert_equal 301, status
     assert_equal 'https://utakatanka.jp/posts/1?ref=share', headers.fetch('location')
   end
 
   it '正規ドメインへのアクセスはそのまま処理する' do
-    status, headers = middleware.call(Rack::MockRequest.env_for('https://utakatanka.jp/posts/1?ref=share'))
+    status, headers = @middleware.call(Rack::MockRequest.env_for('https://utakatanka.jp/posts/1?ref=share'))
 
     assert_equal 200, status
     assert_equal 'application/test', headers.fetch('content-type')
   end
 
   it 'Heroku以外のドメインへのアクセスはそのまま処理する' do
-    status, = middleware.call(Rack::MockRequest.env_for('https://example.com/posts/1'))
+    status, = @middleware.call(Rack::MockRequest.env_for('https://example.com/posts/1'))
 
     assert_equal 200, status
   end
