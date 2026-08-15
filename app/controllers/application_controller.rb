66ab5149aa251e50bf56451d2888536afa16a708
fuyu77 <kiwi.vc@gmail.com>
2026-08-15 21:33:05 +0900

    不要なメソッドを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :ensure_domain
-
-  FQDN = 'utakatanka.jp'
-
-  def ensure_domain
-    return unless /\.herokuapp.com/.match?(request.host)
-
-    port = ":#{request.port}" unless [80, 443].include?(request.port)
-    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
-  end
 
   protected
 
